ec0325b0d6064ba21012135de9e28812bbb2ac3e
sidhtc510 <sidhtc510@gmail.com>
2023-03-09 20:23:30 +0100

    fetch promise

---
+++ lesson13_promise/scripts/script.js
@@ -0,0 +1,43 @@
+const link = "https://jsonplaceholder.typicode.com/posts";
+
+const posts = document.querySelector(".posts");
+
+function createPost(title, body) {
+    const container = document.createElement("div");
+    const pTitle = document.createElement("p");
+    const pBody = document.createElement("p");
+    pTitle.innerText = title;
+    pBody.innerText = body;
+  
+    pTitle.classList.add("title");
+    pBody.classList.add("text");
+  
+    container.append(pTitle, pBody);
+  
+    return container;
+  }  
+
+fetch(link).then(function (resp) {
+  resp.json().then(function (data) {
+    data.forEach(function ({ title, body }) {
+      // console.log(elem);
+      const newPost = createPost(title, body);
+      posts.append(newPost);
+    });
+  });
+});
+
+// const request = new Promise((resolve, reject) => {
+//   const number = Math.random();
+//   number < 0.5 ? resolve(number) : reject(number);
+// });
+
+// request.then(
+//   function (value) {
+//     console.log("positive " + value);
+//   },
+//   function (value) {
+//     console.log("negative " + value);
+//   }
+// );
+
