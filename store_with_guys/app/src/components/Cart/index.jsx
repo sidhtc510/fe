22a462815900ed1d9d3c8e769acf015f9c543795
sidhtc510 <sidhtc510@gmail.com>
2023-06-03 01:02:40 +0200

    .

--- store_with_guys/app/src/components/Cart/index.jsx
+++ store_with_guys/app/src/components/Cart/index.jsx
@@ -2,35 +2,43 @@ import { useSelector } from "react-redux";
 import s from "./style.module.css";
 
 function ProductInCart({ id, product, price, image, count }) {
-  const defaultImg = !image ? "./images/noImage.webp" : "./images/" + image;
-  return (
-    <div className={s.product}>
-        <h2>{product}</h2>
-      <div>
-      <img src={defaultImg} />
-      </div>
-        <h3>{price}$</h3>
-       
-        <button>-</button>
-        <input type="number" value={count}/>
-        <button>+</button>
-    </div>
-  );
+      const defaultImg = !image ? "./images/noImage.webp" : "./images/" + image;
+      return (
+            <div className={s.product}>
+                  <h2>{product}</h2>
+                  <div>
+                        <img src={defaultImg} />
+                  </div>
+                  <span className={s.priceWrap}>
+                        <p>Price</p>
+                        <h3>{price}$</h3>
+                  </span>
+                  <div>
+                        <button>-</button>
+                        <input type="number" defaultValue={count} />
+                        <button>+</button>
+                  </div>
+                  <span  className={s.amountWrap}>
+                        <p>Amount</p>
+                        <h3>{price * count}$</h3>
+                  </span>
+            </div>
+      );
 }
 
 export function Cart() {
-  const { cart, goods } = useSelector((state) => state);
+      const { cart, goods } = useSelector((state) => state);
 
-  const goodsInTheCart = cart.map((item) => {
-    const product = goods.find((el) => el.id === item.id);
-    return { ...item, ...product };
-  });
+      const goodsInTheCart = cart.map((item) => {
+            const product = goods.find((el) => el.id === item.id);
+            return { ...item, ...product };
+      });
 
-  return (
-    <div className={s.productsWrapper}>
-      {goodsInTheCart.map((data) => (
-        <ProductInCart {...data} key={data.id} />
-      ))}
-    </div>
-  );
+      return (
+            <div className={s.productsWrapper}>
+                  {goodsInTheCart.map((data) => (
+                        <ProductInCart {...data} key={data.id} />
+                  ))}
+            </div>
+      );
 }
--- store_with_guys/app/src/components/CategoryWrapper/PriceRange/index.jsx
+++ store_with_guys/app/src/components/CategoryWrapper/PriceRange/index.jsx
@@ -48,6 +48,7 @@ export default function PriceRange() {
                                     type="checkbox"
                                     name="isInStock"
                                     id="isInStock"
+                                    defaultChecked
                               />
                         </label>
                   </div>
