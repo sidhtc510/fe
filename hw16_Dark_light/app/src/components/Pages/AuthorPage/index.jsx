e112afde7ed432e8e3abb21cffcf1f5fc73b983d
sidhtc510 <sidhtc510@gmail.com>
2023-06-18 15:50:44 +0200

    hw16 - about author

--- hw16_Dark_light/app/src/components/Pages/AuthorPage/index.jsx
+++ hw16_Dark_light/app/src/components/Pages/AuthorPage/index.jsx
@@ -4,27 +4,64 @@ import s from "./s.module.css";
 import BookForSale from "../../UI/BookForSale";
 import img1 from "./images/1.png";
 import img2 from "./images/2.png";
+import img_person from "./images/person.png";
 import MainWrapper from "../../UI/MainWrapper";
 
 export default function AuthorPage() {
     return (
-        <MainWrapper color_main="white">
-            {/* <div className={s.author_page_wrapper}> */}
-            <TitleOfPage>The Author’s Book</TitleOfPage>
+        <>
+            <MainWrapper color_main="white">
+                {/* <div className={s.author_page_wrapper}> */}
+                <TitleOfPage>The Author’s Book</TitleOfPage>
 
-            <div className={s.items_container}>
-                <BookForSale
-                    src={img1}
-                    title="Atomic One’s"
-                    price="$ 13.84 USD"
-                />
-                <BookForSale
-                    src={img2}
-                    title="The Dark Light"
-                    price="$ 86.11 USD"
-                />
-            </div>
-            {/* </div> */}
-        </MainWrapper>
+                <div className={s.items_container}>
+                    <BookForSale
+                        src={img1}
+                        title="Atomic One’s"
+                        price="$ 13.84 USD"
+                    />
+                    <BookForSale
+                        src={img2}
+                        title="The Dark Light"
+                        price="$ 86.11 USD"
+                    />
+                </div>
+                {/* </div> */}
+            </MainWrapper>
+
+            <MainWrapper color_main="#F5F8FC">
+                <div className={s.about_block_wrapper}>
+                    <div className={s.img_wrap}>
+                        <img src={img_person} alt="" />
+                    </div>
+                    <div>
+                        <TitleOfPage className={s.span_line_to_left}>
+                            About the Author
+                        </TitleOfPage>
+                        <p className={s.info}>
+                            We believe that bookstores are essential to a
+                            healthy culture. They’re where authors can connect
+                            with readers, where we discover new writers, where
+                            children get hooked on the thrill of reading that
+                            can last a lifetime.
+                        </p>
+                        <div className={s.achievements}>
+                            <p>
+                                <p>02</p>
+                                <span>Books Published</span>
+                            </p>
+                            <p>
+                                <p>4.5</p>
+                                <span>User Reviews</span>
+                            </p>
+                            <p>
+                                <p>04</p>
+                                <span>Best Seller Awards</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </MainWrapper>
+        </>
     );
 }
--- hw16_Dark_light/app/src/components/UI/TitleOfPage/index.jsx
+++ hw16_Dark_light/app/src/components/UI/TitleOfPage/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import s from "./s.module.css";
 
-export default function TitleOfPage({ children }) {
+export default function TitleOfPage({ children, className }) {
     return (
-        <div className={s.wrap}>
+        <div className={[s.wrap, className].join(" ")}>
             <p>{children}</p>
             <span></span>
         </div>
