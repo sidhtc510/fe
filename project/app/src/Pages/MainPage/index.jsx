6e2cc968f61b63ec7441094755e35756b66eec1f
sidhtc510 <sidhtc510@gmail.com>
2023-07-04 23:58:17 +0200

    sch

--- project/app/src/Pages/MainPage/index.jsx
+++ project/app/src/Pages/MainPage/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Wrapper from "../../components/UI/Wrapper";
+import Banner from "../../components/Banner";
 
 export default function MainPage() {
     return (
         <Wrapper>
-            <p>Main page</p>
+            <Banner />
         </Wrapper>
     );
 }
---
+++ project/app/src/components/Banner/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import s from "./s.module.css";
+import banner from "./banner.jpg";
+import Button from "../UI/Button";
+import { Link } from "react-router-dom";
+
+export default function Banner() {
+    return (
+        <div
+            className={s.wrapper}
+            style={{ backgroundImage: `url(${banner})` }}
+        >
+            <div className={s.info}>
+                <h2>SALE</h2>
+                <h3>New season</h3>
+                <Link to='/sales'>
+                <Button className="BannerSale">Sale</Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
