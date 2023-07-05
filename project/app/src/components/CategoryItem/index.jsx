58a15f7729a5c4ced85f52e75cf0168b4e33c59a
sidhtc510 <sidhtc510@gmail.com>
2023-07-06 01:02:01 +0200

    some changes

--- project/app/src/components/CategoryItem/index.jsx
+++ project/app/src/components/CategoryItem/index.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 export default function CategoryItem({ id, title, image }) {
     return (
-            <Link to={`/categories/${id}`}>
-        <div className={s.catWrap}>
+        <Link to={`/categories/${id}`}>
+            <div className={s.catWrap}>
                 <img src={`http://localhost:3333/${image}`} alt="" />
                 <p>{title}</p>
-        </div>
-            </Link>
+            </div>
+        </Link>
     );
 }
