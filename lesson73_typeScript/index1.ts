91bb63abac5a638e75219b4cca1035e919e0635e
sidhtc510 <sidhtc510@gmail.com>
2023-10-19 00:17:52 +0200

    .

--- lesson73_typeScript/index1.ts
+++ lesson73_typeScript/index1.ts
@@ -8,7 +8,7 @@ const fetchCurr = (response: string): number => {
 function transferEurToUsd(available: boolean, amount: number, commission: number): void {
     if (available) {
         let res: number = fetchCurr(currRate) * amount * commission;
-        console.log(res);
+        // console.log(res);
         // Или запись в элемент на странице вместо консоли
     } else {
         console.log("Сейчас обмен недоступен");
@@ -16,3 +16,24 @@ function transferEurToUsd(available: boolean, amount: number, commission: number
 }
 
 transferEurToUsd(true, 500, 1.05);
+
+const departments: string[] = ["dev", "design", "marketing"];
+const num: number[] = [2, 4, 1];
+const numArray: number[][] = [
+    [2, 4, 1],
+    [12, 3, 2],
+];
+
+const report = departments.filter((d: string) => d !== "dev").map((d: string) => `${d} - done`);
+const [first] = report;
+
+const userDataTuple: [boolean, number, ...string[]] = [true, 40, "John", "tolik"];
+
+const message: string | number = 4;
+const messages: string[] | number[] = [443, 221];
+
+const printMsg = (msg: string | number): string | number => {
+    return msg
+};
+console.log(printMsg("testt"));
+
