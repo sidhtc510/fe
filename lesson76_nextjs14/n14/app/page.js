c51cb08dc91a84cf8955a81a76a44e322af3aeb9
sidhtc510 <sidhtc510@gmail.com>
2023-11-03 14:55:22 +0100

    .

--- lesson76_nextjs14/n14/app/page.js
+++ lesson76_nextjs14/n14/app/page.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
         <main>
             <h2>Home Page</h2>
+            <Image 
+            src='/4.avif'
+            width={300}
+            height={550}
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+            />
         </main>
     );
 }
