4d53efbf0a609bd1334999968ac79fe25e331310
Anatoliy Sidorenko <sidhtc510@gmail.com>
2025-08-14 21:14:46 +0200

    .

--- ipfs_crud/src/ipfsClients.js
+++ ipfs_crud/src/ipfsClients.js
@@ -1,9 +1,5 @@
 import { create } from 'ipfs-http-client';
 
-// Локальная нода (запусти: ipfs daemon)
-// export const localNode = create({ url: 'http://127.0.0.1:5001' });
-
-// Gateway (Infura как пример)
 const projectId = 'Bd5RmA6zPcOi6L598fOvZsldO9PLRkhLcTG9YKLhHnHX9AXiadB95g';
 const projectSecret = '6c36d4476ceb421780d8a1bd1d66bae3';
 const auth = 'Basic ' + btoa(projectId + ':' + projectSecret);
